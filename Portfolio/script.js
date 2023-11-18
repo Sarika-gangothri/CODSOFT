73ed9938b5ac8ab75995871ade2b61ee1ae05e24
Sarika-gangothri <150992166+Sarika-gangothri@users.noreply.github.com>
2023-11-18 22:55:14 +0530

    Create script.js

---
+++ Portfolio/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    if(document.getElementById("sidenav").style.display=='block'){
+        document.getElementById("sidenav").style.display='none';
+    }else{
+        document.getElementById("sidenav").style.display='block'
+    }
+}
+function closeNav(){
+    document.getElementById("sidenav").style.display='none';
+}
